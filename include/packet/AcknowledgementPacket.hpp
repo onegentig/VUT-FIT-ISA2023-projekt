8613593423983ef86fcfba822cee766dcfd2ce2b
onegen <84882649+onegentig@users.noreply.github.com>
2023-10-08 02:06:27 +0200

    refactor(core): slightly improve code and comments of RQ & ACK

--- include/packet/AcknowledgementPacket.hpp
+++ include/packet/AcknowledgementPacket.hpp
@@ -12,35 +12,57 @@
 
 /**
  * @brief TFTP acknowledgement packet class.
+ * Represents the ACK packet (opcode 4) in TFTP, which is the expected
+ * response for WRQ and DATA packets (when not erroneous). This packet type only
+ * contains the opcode and the number of the block it’s acknowledging (when not
+ * applicable, ex. WRQ response, it’s 0).
+ * @see https://datatracker.ietf.org/doc/html/rfc1350#autoid-5
  */
 class AcknowledgementPacket : public BasePacket {
    public:
      /* === Constructors === */
 
      /**
-      * @brief Constructs a new Acknowledgement packet object.
+      * @brief Constructs a new empty ACK packet object.
       * @return AcknowledgementPacket
       */
      AcknowledgementPacket();
 
      /**
-      * @brief Constructs a new Acknowledgement packet object.
-      * @param uint16_t - block number
+      * @brief Constructs a new ACK packet object with set block number.
+      * @param uint16_t block number
       * @return AcknowledgementPacket
       */
      explicit AcknowledgementPacket(uint16_t blockN);
 
+     /**
+      * @brief Checks equality of two ACK packets.
+      * @param other ACK packet to compare with
+      * @return true when equal,
+      * @return false when not equal
+      */
+     bool operator==(const AcknowledgementPacket& other) const {
+          return this->opcode == other.opcode && this->blockN == other.blockN;
+     }
+
      /* === Core Methods === */
 
      /**
-      * @brief Returns the binary representation of the packet.
+      * @brief Returns binary representation of the packet.
+      * Creates binary vector of the packet. ACK packets always have 2B
+      * opcode and 2B block number and nothing else. Block number is 0 when
+      * acknowledging a WRQ, otherwise 1+ (as DATA blocks are indexed from 1).
+      *
       * @return std::vector<char> - packet in binary
       */
      std::vector<char> toBinary() const override;
 
      /**
-      * @brief Creates a new packet from a binary representation.
-      * @param std::vector<char> - packet in binary
+      * @brief Creates an ACK packet from a binary representation.
+      * Attempts to parse a binary vector into an ACK packet.
+      * @throws std::invalid_argument when vector is not a proper ACK packet
+      *
+      * @param std::vector<char> packet in binary
       * @return void
       */
      void fromBinary(const std::vector<char>& binaryData) override;
@@ -55,7 +77,7 @@ class AcknowledgementPacket : public BasePacket {
 
      /**
       * @brief Sets the two-byte block number.
-      * @param uint16_t - block number
+      * @param uint16_t block number
       * @return void
       */
      void setBlockNumber(uint16_t blockN) { this->blockN = blockN; }
